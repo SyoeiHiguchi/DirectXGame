cd3f9978ca06d957b3bbaa78203151c5ea4cf7ec
SyoeiHiguchi <k021g1228@g.neec.ac.jp>
2022-04-11 17:47:08 +0900

    01_01_確認課題

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow("GE2A_13_ヒグチショウエイ_AL3");
+	win->CreateGameWindow("GE2A_13_ヒグチ_ショウエイ_AL3");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
